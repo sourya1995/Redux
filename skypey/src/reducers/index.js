460dfe6cfac8c5cc7b769d39a0ac29d8d232f599
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-06-20 12:18:35 +0530

    reducer composition

--- skypey/src/reducers/index.js
+++ skypey/src/reducers/index.js
@@ -1,3 +1,8 @@
-export default (state, action) =>{
-    return state;
-}
+import user from "./user";
+import contacts from "./contacts";
+import { combineReducers } from "redux";
+
+export default combineReducers({
+    user: user,
+    contacts: contacts //value returned from reducer function is NOT THE STATE, it is just the value of the key represented in the state object
+});
--- skypey/src/store/index.js
+++ skypey/src/store/index.js
@@ -1,5 +1,5 @@
 import { legacy_createStore as createStore } from 'redux';
-import { reducer } from '../reducers';
+import { reducer } from '../reducers/contacts';
 import { contacts } from '../static-data';
-const store = createStore(reducer, {contacts});
+const store = createStore(reducer); //initial state is passed directly, for SSR
 export default store;
